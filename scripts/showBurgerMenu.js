3f882003e180ffdc25466346737fb41bd3be14cf
mcpicornell <marc.castro.picornell@gmail.com>
2023-06-05 19:19:41 +0200

    js function navigate to roomDetails updated

--- scripts/showBurgerMenu.js
+++ scripts/showBurgerMenu.js
@@ -2,20 +2,58 @@ const menu = document.getElementsByClassName("header__inner-li")[0];
 const burger = document.getElementsByClassName("inner__burger")[0];
 
 
-burger.addEventListener("click", (event) =>{
-    if (menu.style.visibility == "visible"){
+burger.addEventListener("click", (event) => {
+    if (menu.style.visibility == "visible") {
         menu.style.visibility = "hidden";
-        
+
     }
-    else{
+    else {
         menu.style.visibility = "visible";
-        
-    } 
+
+    }
 });
 
-const roomElement = document.getElementsByClassName("section-selection-rooms__rooms")[0];
 
-roomElement.addEventListener("click", (event) => {
-    const newUrl = "roomDetails7.html";
+for(i=0; i<document.getElementsByClassName("section-selection-rooms__rooms").length; i++){
+    document.getElementsByClassName("section-selection-rooms__rooms")[i]
+    .addEventListener("click", (event) => {
+        const newUrl = "roomDetails.html";
+        window.location.href = newUrl;
+    })
+}
+
+const headerTag = document.getElementsByClassName("header__inner")[0];
+
+headerTag.addEventListener("click", (event) => {
+    const newUrl = "index.html";
     window.location.href = newUrl;
 })
+
+
+// const aboutUs = document.getElementById("about")[0];
+
+// aboutUs.addEventListener("click", (event) => {
+//     const newUrl = "about.html";
+//     window.location.href = newUrl;
+// })
+
+// const rooms = document.getElementById("rooms")[0];
+
+// rooms.addEventListener("click", (event) => {
+//     const newUrl = "rooms.html";
+//     window.location.href = newUrl;
+// })
+
+// const offers = document.getElementById("offers")[0];
+
+// rooms.addEventListener("click", (event) => {
+//     const newUrl = "offers.html";
+//     window.location.href = newUrl;
+// })
+
+// const contact = document.getElementById("contact")[0];
+
+// rooms.addEventListener("click", (event) => {
+//     const newUrl = "contact.html";
+//     window.location.href = newUrl;
+// })
